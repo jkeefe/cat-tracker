2964af7a9d7e7595bda2bb1f75eadb5865499743
John Keefe <john@johnkeefe.net>
2017-12-04 22:02:15 -0500

    tinkering with ble startup

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 echo "Preparing bluetooth..."
-if ! /usr/bin/hciattach /dev/ttyAMA0 bcm43xx 921600 noflow -; then
-    echo "First try failed. Let's try again..."
-    /usr/bin/hciattach /dev/ttyAMA0 bcm43xx 921600 noflow -
-fi
-
-hciconfig hci0 up
-echo "Bluetooth up"
-
-npm start
+# if ! /usr/bin/hciattach /dev/ttyAMA0 bcm43xx 921600 noflow -; then
+#     echo "First try failed. Let's try again..."
+#     /usr/bin/hciattach /dev/ttyAMA0 bcm43xx 921600 noflow -
+# fi
+# 
+# hciconfig hci0 up
+# echo "Bluetooth up"
+# 
+# npm start
