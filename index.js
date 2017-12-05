41061513c512cbc4c291b749787f221127cf543e
John Keefe <john@johnkeefe.net>
2017-12-04 20:06:37 -0500

    trying new version of js file

--- index.js
+++ index.js
@@ -2,11 +2,12 @@ var noble = require('noble');
 
 noble.startScanning();
 
-noble.on(‘discover’, function(peripheral) { 
+noble.on('discover', function(peripheral) {
 
-  var macAddress = peripheral.uuid;
-  var rss = peripheral.rssi;
-  var localName = advertisement.localName; 
-  console.log('found device: ', macAdress, ' ', localName, ' ', rss);   
-}
+    var macAddress = peripheral.uuid;
+    var rss = peripheral.rssi;
+    var localName = peripheral.advertisement.localName; 
+    console.log("found device: ", macAddress, localName, rss);   
+
+});
 
