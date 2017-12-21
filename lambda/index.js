d133aa0b1faed52012b6b8408c337927ec4b604b
John Keefe <john@johnkeefe.net>
2017-12-21 00:13:03 -0500

    cool - added Sentry alerts

--- lambda/index.js
+++ lambda/index.js
@@ -2,6 +2,8 @@
 const request = require('request');
 const moment = require('moment');
 var mongodb = require('mongodb');
+var Raven = require('raven');
+Raven.config('https://a78f0e250a314ad3857ef18200c57f44:4ac26c677d6f4641afcf4e84b4085955@sentry.io/262409').install();
 
 const mongo_url = "mongodb://" + process.env.MONGO_DB_USER + ":" + process.env.MONGO_DB_USER_PASS + "@ds111818-a0.mlab.com:11818,ds111818-a1.mlab.com:11818/bot-workshop-db?replicaSet=rs-ds111818";
 
@@ -61,7 +63,7 @@ function hitOnePi(url) {
             if (response.statusCode !== 200){
                 console.log('statusCode:', response.statusCode);
                 console.log('body:', response.body);
-                resolve(null);
+                reject(response.statusCode);
                 return;
             }
             
@@ -79,6 +81,17 @@ function storeInDB(data) {
         record.poll_time = moment().utc().format();
         record.devices = {};
         
+        // little routine to throw an "error" if we
+        // see fewer than 3 pi's, alerting Sentry
+        
+        try {
+            if (data.length < 3) {
+                throw new Error("Looks like a pi is offline: See https://dashboard.resin.io/apps/785142/devices");
+            }
+        } catch (e) {
+            Raven.captureException(e);
+        }
+        
         // loop through the device data and build 
         // a json object for inserting into the database
         for (var i = 0; i < data.length; i++) {
@@ -91,7 +104,7 @@ function storeInDB(data) {
             
             if (err) {
                 console.log("Error opening database:", err);
-                reject();
+                reject("Error opening database:");
                 return;
             }
             
@@ -103,7 +116,7 @@ function storeInDB(data) {
                 
                 if (error) {
                     console.log("Error opening database:", error);
-                    reject();
+                    reject("Error opening database.");
                     return;
                 }
                 
